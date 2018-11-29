1ed63fa7a325a65c0f0b0ea07cd4d0261a1207fd
soeon kim <developeon80@gmail.com>
2018-11-29 22:51:01 +0900

    Code Indenting 규칙에 맞게 수정

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,16 +1,18 @@
 <?php
 class MY_Controller extends CI_Controller {
 
-    function _construct() {
+    function _construct() 
+    {
         parent::_construct();
     }
 
-    function _header() {
+    function _header() 
+    {
         $this->load->view('header');
     }
 
-    function _footer() {
+    function _footer() 
+    {
         $this->load->view('footer');
     }
-
 }
