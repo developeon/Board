9f54778c34e6a6c29cd1440c901ddb13ac20aadc
soeon kim <developeon80@gmail.com>
2018-11-29 21:32:47 +0900

    index.php 제거 및 hedaer, footer 생성

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,10 @@
-    <script type="text/javascript" src="/includes/js/bootstrap.min.js">
+    <footer class="footer">
+        <div class="container">
+            <span class="text-muted">Place sticky footer content here.</span>
+        </div>
+    </footer>
+    <script type="text/javascript" src="/includes/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="/includes/js/popper.min.js"></script>
+    <script type="text/javascript" src="/includes/js/bootstrap.min.js"></script>
 </body>
 </html>
