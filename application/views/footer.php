a70975f016a5cbc71928c1da743a1730b89e6197
soeon kim <developeon80@gmail.com>
2018-11-29 20:21:52 +0900

    초기 설정

---
+++ application/views/footer.php
@@ -0,0 +1,3 @@
+    <script type="text/javascript" src="/includes/js/bootstrap.min.js">
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>더팀스(THE TEAMS)</title>
+    <link rel="stylesheet" type="text/css" href="/includes/css/bootstrap.min.css">
+</head>
+<body>
